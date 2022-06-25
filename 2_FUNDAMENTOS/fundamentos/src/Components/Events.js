2ca6f54e543a3d0f1f3042130f01e186f8624aae
Angela Caldas <aes.caldas@gmail.com>
2022-06-25 18:29:37 -0300

    testing other ways of creating click events

--- 2_FUNDAMENTOS/fundamentos/src/Components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/Components/Events.js
@@ -7,6 +7,12 @@ const Events = () => {
     <div>
       <div>
         <button onClick={handleMyEvent}>Clique aqui!</button>
+        <button onClick={() => { console.log('clicou'); }}>Clique aqui também!</button>
+        <button onClick={() => {
+          if (true) {
+            console.log('Isso não deveria existir =)');
+          }
+        }}>Clica aqui, por favor.</button>
       </div>
     </div>
   );
