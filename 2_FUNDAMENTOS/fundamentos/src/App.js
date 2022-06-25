361b2bdde85e7f579b9240c48f1ce0daad787e02
Angela Caldas <aes.caldas@gmail.com>
2022-06-25 16:06:34 -0300

    feat: first component

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,11 +1,18 @@
+// components
+import FirstComponent from './Components/FirstComponent';
+
+// styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+
+      {/* First component loaded into App.js */}
+      <FirstComponent />
     </div>
   );
-}
+};
 
 export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/Components/FirstComponent.js
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+    </div>
+  );
+};
+
+export default FirstComponent;
