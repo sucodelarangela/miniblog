e9ec364120bb304c23010561b613f8dcbd07d35a
Angela Caldas <aes.caldas@gmail.com>
2022-06-25 18:53:50 -0300

    refactor: deleting unnecessary code

--- 2_FUNDAMENTOS/fundamentos/src/Components/Challenge.js
+++ 2_FUNDAMENTOS/fundamentos/src/Components/Challenge.js
@@ -11,7 +11,7 @@ const Challenge = () => {
       <h4>O primeiro valor a ser somado é o número {firstValue}.</h4>
       <h4>O segundo valor a ser somado é o número {secondValue}.</h4>
       <p>Clique no botão a seguir e veja o resultado no console do navegador</p>
-      <button onClick={() => handleSum()}>Somar valores</button>
+      <button onClick={handleSum}>Somar valores</button>
     </div>
   );
 };
