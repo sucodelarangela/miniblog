bac718bfb0a8376bc128dcef0b9f0df4956c0338
Angela Caldas <aes.caldas@gmail.com>
2022-06-25 19:25:59 -0300

    refactor: dealing with images

--- 3_AVANCANDO_NO_REACT/avancando-react/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando-react/src/App.js
@@ -1,11 +1,30 @@
+// styles
 import './App.css';
+
+// images
+import City from './assets/city.jpg';
 
 function App() {
   return (
     <div className="App">
-      <h1>Seção 3: Avançando no React</h1>
+      <h1>Avançando em React</h1>
+      {/* Image in /public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Image in src */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+The src in img tag is directly linked to /public, so it's not necessary to use "/public" on the src file path. Useful for static images.
+When images are inside /src or in a child folder, we can use import instead of the img tag. In this case, we'll have a dynamic src and these imports may be used as variables.
+
+*/
