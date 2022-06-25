8e897dc139b4a2b59e176c0f20d65009b85dfb0d
Angela Caldas <aes.caldas@gmail.com>
2022-06-25 18:33:41 -0300

    rendering JSX from functions

--- 2_FUNDAMENTOS/fundamentos/src/Components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/Components/Events.js
@@ -1,6 +1,14 @@
 const Events = () => {
-  const handleMyEvent = (e) => {
-    console.log(e);
+  const handleMyEvent = () => {
+    console.log('Ativou o evento');
+  };
+
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando isso!</h1>;
+    } else {
+      return <h1>Também posso renderizar isso!</h1>;
+    }
   };
 
   return (
@@ -14,6 +22,8 @@ const Events = () => {
           }
         }}>Clica aqui, por favor.</button>
       </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   );
 };
