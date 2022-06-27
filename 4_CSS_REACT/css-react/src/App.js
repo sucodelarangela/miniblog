1fa1fa6b10c17f890a0a13ba2ed3efdd396100a3
Angela Caldas <aes.caldas@gmail.com>
2022-06-27 20:00:14 -0300

    initial commit: css in react

---
+++ 4_CSS_REACT/css-react/src/App.js
@@ -0,0 +1,17 @@
+// styles
+import './App.css';
+
+// components
+import MyComponent from './Components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CSS com React</h1>
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 4_CSS_REACT/css-react/src/Components/MyComponent.js
@@ -0,0 +1,14 @@
+// styles
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>CSS de componente</h1>
+      <p>Este é o parágrafo do componente</p>
+      <p className="my-comp-paragraph">Este é o meu componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
