7051de87d6e46957b8ca5d4a35fdadb8f963b13f
Angela Caldas <aes.caldas@gmail.com>
2022-06-28 20:13:29 -0300

    dynamic classes

--- 4_CSS_REACT/css-react/src/App.js
+++ 4_CSS_REACT/css-react/src/App.js
@@ -11,6 +11,8 @@ function App() {
 
   const [name] = useState("Matheus");
 
+  const [redTitle] = useState(true);
+
   return (
     <div className="App">
       <h1>CSS com React</h1>
@@ -23,6 +25,9 @@ function App() {
       <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2>
       <h2 style={name == "Matheus" ? ({ color: "green", backgroundColor: "#000000" }) : null}>CSS dinâmico</h2>
       <h2 style={name != "Matheus" ? ({ color: "green", backgroundColor: "#000000" }) : null}>CSS dinâmico</h2>
+      {/* dynamic classes */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+      <h2 className={!redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
     </div>
   );
 }
