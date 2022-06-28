b9a72272cbc3674fe0fc52e96f72058eecc0e8a7
Angela Caldas <aes.caldas@gmail.com>
2022-06-28 20:08:19 -0300

    testing inline css

--- 4_CSS_REACT/css-react/src/App.js
+++ 4_CSS_REACT/css-react/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // styles
 import './App.css';
 
@@ -5,11 +7,22 @@ import './App.css';
 import MyComponent from './Components/MyComponent';
 
 function App() {
+  const n = 15;
+
+  const [name] = useState("Matheus");
+
   return (
     <div className="App">
       <h1>CSS com React</h1>
       <MyComponent />
       <p>Este parágrafo é do App.js</p>
+      {/* Inline css */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "4px solid red" }}>Este elemento foi estilizado de forma inline.</p>
+      {/* Dynamic inline css */}
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2>
+      <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2>
+      <h2 style={name == "Matheus" ? ({ color: "green", backgroundColor: "#000000" }) : null}>CSS dinâmico</h2>
+      <h2 style={name != "Matheus" ? ({ color: "green", backgroundColor: "#000000" }) : null}>CSS dinâmico</h2>
     </div>
   );
 }
