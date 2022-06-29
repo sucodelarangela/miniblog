1cc9216e6a6cced8fc54d696a0e360f319a55561
Angela Caldas <aes.caldas@gmail.com>
2022-06-29 15:23:40 -0300

    feat: start screen

---
+++ 6_SECRET_WORD/secretword/src/Components/StartScreen.js
@@ -0,0 +1,14 @@
+// styles
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
