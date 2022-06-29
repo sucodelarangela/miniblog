b006a355fae1dc13e2161e5695f617493ad26f39
Angela Caldas <aes.caldas@gmail.com>
2022-06-29 09:52:43 -0300

    ways of retrieving input data

--- 5_FORM_REACT/forms/src/Components/MyForm.js
+++ 5_FORM_REACT/forms/src/Components/MyForm.js
@@ -1,6 +1,21 @@
 import './MyForm.css';
+import { useState } from 'react';
 
 const MyForm = () => {
+  // manipulating data
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  // 1st way of retrieving input values.
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name)
+  console.log(email)
+
+  // 2nd way of retrieving input values is using the useState inline: it may be cleaner if you have too many inputs (see email input). It's used simply for getting data. If you have other kinds of manipulation, it's better to create a function
+
   return (
     <div>
       {/* Creating forms */}
@@ -11,13 +26,14 @@ const MyForm = () => {
             type="text"
             name="name"
             id="name"
+            onChange={handleName}
             placeholder="Digite o seu nome"
           />
           {/* Input inside label */}
           <label>
             E-mail:
             {/* <span>E-mail:</span> --- Using span is not good for accessibility*/}
-            <input type="email" name="email" placeholder="Digite seu e-mail" />
+            <input type="email" name="email" placeholder="Digite seu e-mail" onChange={(e) => setEmail(e.target.value)} />
           </label>
         </div>
         <input type="submit" value="Enviar" />
