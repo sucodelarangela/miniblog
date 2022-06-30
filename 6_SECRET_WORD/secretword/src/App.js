fbc9f86205ff191b1d708a2bce52b017d486bbd2
Angela Caldas <aes.caldas@gmail.com>
2022-06-30 17:03:35 -0300

    randomly picking words and categories

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -22,10 +22,36 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState('')
+  const [pickedCategory, setPickedCategory] = useState('')
+  const [letters, setLetters] = useState('')
+
+  // pickind word and category
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { word, category }
+  }
 
   // Starting the game
   const startGame = () => {
-    setGameStage(stages[1].name)
+    // destructuring
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split('')
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    // fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(wordLetters)
+    setGameStage(stages[1].name);
   }
 
   // Processing the letter input
