51251d45b18e0f76476c94326db8295c875ca30a
Angela Caldas <aes.caldas@gmail.com>
2022-07-01 13:12:25 -0300

    defining states of game component

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -25,6 +25,10 @@ function App() {
   const [pickedWord, setPickedWord] = useState('')
   const [pickedCategory, setPickedCategory] = useState('')
   const [letters, setLetters] = useState('')
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
 
   // pickind word and category
   const pickWordAndCategory = () => {
@@ -68,7 +72,16 @@ function App() {
     <div className="App">
       {/* Conditionally shows the page accordingly to the gameStage */}
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score} />)}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
--- 6_SECRET_WORD/secretword/src/Components/Game.js
+++ 6_SECRET_WORD/secretword/src/Components/Game.js
@@ -1,19 +1,25 @@
 // styles
 import './Game.css'
 
-const Game = ({ verifyLetter }) => {
+const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
   return (
     <div className="game">
       <p className="point">
-        <span>Pontuação: 000</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className="tip">
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
+      <p>Você ainda tem {guesses} tentativa(s)</p>
       <div className="wordContainer">
-        <span className='letter'>A</span>
-        <span className='blankSquare'></span>
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? (
+            <span className='letter'>{letter}</span>
+          ) : (
+            <span key={i} className='blankSquare'></span>
+          )
+        ))}
       </div>
       <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
@@ -24,10 +30,11 @@ const Game = ({ verifyLetter }) => {
       </div>
       <div className="wrongLetterContainer">
         <p>Letras já utilizadas:</p>
-        <span>a, </span>
-        <span>b, </span>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter}, </span>
+        ))}
       </div>
-    </div>
+    </div >
   )
 }
 
