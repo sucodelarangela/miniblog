be5b93a5ca59ff8c853a838c5b1c4524bf7be91b
Angela Caldas <aes.caldas@gmail.com>
2022-07-01 12:54:09 -0300

    basic structure and styles of game component

--- 6_SECRET_WORD/secretword/src/Components/Game.js
+++ 6_SECRET_WORD/secretword/src/Components/Game.js
@@ -3,9 +3,30 @@ import './Game.css'
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="point">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className='blankSquare'></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letter' maxLength='1' required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
