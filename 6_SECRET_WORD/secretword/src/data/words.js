5dafcf5977c433d14d28637a642a49e50cfc3bf0
Angela Caldas <aes.caldas@gmail.com>
2022-07-02 17:38:59 -0300

    increase words list

--- 6_SECRET_WORD/secretword/src/data/words.js
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -1,8 +1,8 @@
 export const wordsList = {
-  carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"],
-  fruta: ["Banana", "Maca", "Pera", "Mamão", "Laranja"],
-  corpo: ["Braço", "Perna", "Cerebro", "Pescoco", "Olhos"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React"],
-  alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+  carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Farol", "Calota", "Volante"],
+  fruta: ["Banana", "Maca", "Pera", "Mamão", "Laranja", "Cupuacu", "Graviola", "Maracuja"],
+  corpo: ["Braco", "Perna", "Cerebro", "Pescoco", "Olhos", "Barriga", "Panturrilha", "Nadegas"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Webcam", "Sistema", "Driver"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "TypeScript", "MongoDB", "Angular"],
+  alimento: ["Arroz", "Feijao", "Carne", "Leite", "Farinha", "Tapioca", "Tomate", "Coentro"],
 };
