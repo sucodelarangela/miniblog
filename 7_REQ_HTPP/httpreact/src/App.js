f035ca6bc65d82dfccc7293967aed195ceea694e
Angela Caldas <aes.caldas@gmail.com>
2022-07-12 12:54:53 -0300

    fetching data with async/await

--- 7_REQ_HTPP/httpreact/src/App.js
+++ 7_REQ_HTPP/httpreact/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [products, setProducts] = useState([]);
+  const url = 'http://localhost:3000/products';
+
+  // retrieving data
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
