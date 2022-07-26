0fb6e03af2df333f2ae722e9719aef4d052d42dd
Angela Caldas <aes.caldas@gmail.com>
2022-07-26 14:51:44 -0300

    :bulb: add comments on source code

--- 7_REQ_HTPP/httpreact/src/App.js
+++ 7_REQ_HTPP/httpreact/src/App.js
@@ -4,9 +4,11 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
   const url = 'http://localhost:3000/products';
 
-  // retrieving data
+  // retrieving data. it happens only once because dependency array is empty
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
@@ -19,14 +21,57 @@ function App() {
     fetchData();
   }, []);
 
+  // adding new products
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    });
+
+
+  };
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       <ul>
+        {/* showing products fecthed from api on page */}
         {products.map((product) => (
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      {/* form to add new products */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
