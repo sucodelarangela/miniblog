5394cc414f5e6199b975127528586a7bc9067185
Angela Caldas <aes.caldas@gmail.com>
2022-09-17 12:46:23 -0300

    sending user to context

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,6 +1,11 @@
 import './App.sass';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useAuthentication } from './hooks/useAuthentication';
+import { useEffect, useState } from 'react';
+
+// components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -15,9 +20,28 @@ import Register from './pages/Register';
 
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  // if user is undefined, then we are still loading data.
+  const loadingUser = user === undefined;
+
+  // every time we have an auth change, we have a useEffect to execute the user change
+  useEffect(() => {
+    // even if we don't have a user, we will receive something different than undefined
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  // if user is still loading (undefined) we show only a p tag with a loading message
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <main className="App">
-      <AuthProvider>
+      <AuthProvider value={user}>
         <Router>
           <Navbar />
           <div className="container">
--- 10_MINIBLOG/miniblog/src/context/AuthContext.js
+++ 10_MINIBLOG/miniblog/src/context/AuthContext.js
@@ -4,6 +4,7 @@ import { createContext, useContext } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children, value }) {
+    // value will receive the logged user
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
