ede4a64470a2b2cd3186084594733e6fc7194c8d
Angela Caldas <aes.caldas@gmail.com>
2022-09-21 18:00:51 -0300

    basic home page structure

--- 10_MINIBLOG/miniblog/src/pages/Home/index.js
+++ 10_MINIBLOG/miniblog/src/pages/Home/index.js
@@ -1,10 +1,33 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
 // styles
-// import styles from './Home.module.sass';
+import styles from './Home.module.sass';
 
 const Home = () => {
+    const [query, setQuery] = useState('');
+    const [posts] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.home}>
+            <h1>Veja os nossos posts mais recentes</h1>
+            <form className={styles.search_form}>
+                <input type="text" placeholder='Ou busque por tags...' onChange={e => setQuery(e.target.value)} />
+                <button className='btn btn-dark'>Pesquisar</button>
+            </form>
+            <div>
+                <h1>Posts...</h1>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados posts</p>
+                        <Link to='/posts/create' className='btn'>Criar primeiro post</Link>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
