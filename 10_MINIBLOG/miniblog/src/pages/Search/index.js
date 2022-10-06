966033d43c6abb0730e2b830b41daaad89943993
Angela Caldas <aes.caldas@gmail.com>
2022-10-06 20:55:07 -0300

    showing search results

--- 10_MINIBLOG/miniblog/src/pages/Search/index.js
+++ 10_MINIBLOG/miniblog/src/pages/Search/index.js
@@ -1,16 +1,32 @@
+import { Link } from 'react-router-dom';
 import { useFetchDocs } from '../../hooks/useFetchDocs';
 import { useQuery } from '../../hooks/useQuery';
 
 import styles from './Search.module.sass';
 
+import PostDetail from '../../components/PostDetail';
+
 const Search = () => {
     const query = useQuery();
     const search = query.get('q'); //get method comes from URLSearchParams
 
+    const { docs: posts } = useFetchDocs('posts', search);
+
     return (
-        <div>
-            <h2>Search</h2>
-            <p>{search}</p>
+        <div className={styles.search_container}>
+            <h2>Resultados da busca</h2>
+            <div>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados posts a partir da sua busca...</p>
+                        <Link to='/' className='btn btn-dark'>Voltar</Link>
+                    </div>
+                )}
+                {/* loading searched posts on page */}
+                {posts && posts.map((post) => (
+                    <PostDetail key={post.id} post={post} />
+                ))}
+            </div>
         </div>
     );
 };
