d87f82b4bb3d0ab830f1b1e7909631ea9f58e9c7
Angela Caldas <aes.caldas@gmail.com>
2022-10-11 19:15:51 -0300

    feat: create react app

---
+++ 11_HOOKS/reacthooks/src/App.js
@@ -0,0 +1,13 @@
+import 'App.css';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      TESTE
+    </div>
+  );
+}
+
+export default App;
