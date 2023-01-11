08db6c906ac1fa150ae3052059604522451571ba
Angela Caldas <aes.caldas@gmail.com>
2023-01-11 12:24:42 -0300

    feat: create and style navbar

--- 18_MOVIES_LIB/movies_lib/src/App.jsx
+++ 18_MOVIES_LIB/movies_lib/src/App.jsx
@@ -1,21 +1,17 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import './App.css';
+
+import { Navbar } from './components/Navbar';
 
 function App() {
 
   return (
-    <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to='/'>Movies Lib</Link>
-        </h2>
-        <Link to='movie/1'>Movie</Link>
-        <Link to='search'>Search</Link>
-      </nav>
+    <section className="App">
+      <Navbar />
       <h2>Movies Lib</h2>
       <Outlet />
-    </div>
+    </section>
   );
 }
 
---
+++ 18_MOVIES_LIB/movies_lib/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
+
+export const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to='/'><BiCameraMovie />Movies Lib</Link>
+      </h2>
+      <form>
+        <input type="text" placeholder='Busque um filme' />
+        <button type='submit'>
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
