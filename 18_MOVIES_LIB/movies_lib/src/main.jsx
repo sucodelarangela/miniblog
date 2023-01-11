e9d228631129f8ba99d6335a29ca793143625996
Angela Caldas <aes.caldas@gmail.com>
2023-01-11 12:17:42 -0300

    feat: config react-router paths

--- 18_MOVIES_LIB/movies_lib/src/main.jsx
+++ 18_MOVIES_LIB/movies_lib/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App';
+import { Home } from './pages/Home';
+import { Movie } from './pages/Movie';
+import { Search } from './pages/Search';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
---
+++ 18_MOVIES_LIB/movies_lib/src/pages/Movie.jsx
@@ -0,0 +1,5 @@
+export const Movie = () => {
+  return (
+    <div>Movie</div>
+  );
+};
