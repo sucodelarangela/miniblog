d8eda9913b924001f0e6bde309b8def383f7a520
Angela Caldas <86853033+sucodelarangela@users.noreply.github.com>
2024-08-15 18:29:31 -0300

    Update PostDetail.js

--- 10_MINIBLOG/miniblog/src/components/PostDetail.js
+++ 10_MINIBLOG/miniblog/src/components/PostDetail.js
@@ -8,14 +8,14 @@ const PostDetail = ({ post }) => {
             <img src={post.image} alt={post.title} />
             <h2>{post.title}</h2>
             <p className={styles.createdby}>{post.createdBy}</p>
-            <div className={styles.tags}>
-                {post.tagsArr.map((tag) => (
-                    <p key={tag}>
-                        <span>#</span>
-                        {tag}
-                    </p>
-                ))}
-            </div>
+            // <div className={styles.tags}>
+            //     {post.tagsArr.map((tag) => (
+            //         <p key={tag}>
+            //             <span>#</span>
+            //             {tag}
+            //         </p>
+            //     ))}
+            // </div>
             <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ler</Link>
         </div>
     );
